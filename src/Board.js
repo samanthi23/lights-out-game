46f89b89afe7b7424292bf90d96964aaf5e84931
Samanthi Jinadasa <baghya.samanthi@gmail.com>
2021-12-06 15:15:29 +0000

    worked on createBoard()

--- src/Board.js
+++ src/Board.js
@@ -30,11 +30,22 @@ import './Board.css';
  **/
 
 class Board extends Component {
+  
+   static defaultProps = {
+      nrows: 5, 
+      ncols: 5, 
+      chanceLightStartsOn: 0.25
+    };
 
   constructor(props) {
     super(props);
+    
 
     // TODO: set initial state
+    this.state = {
+      hasWon: false,
+      board: this.createBoard()
+    };
   }
 
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
@@ -42,6 +53,17 @@ class Board extends Component {
   createBoard() {
     let board = [];
     // TODO: create array-of-arrays of true/false values
+   
+   for(let y =0; y < this.props.nrows; y++ ){
+     let row = [];
+     for (x = 0; x < this.props.ncols; x++){
+       row.push(Math.random() < this.props.chanceLightStartsOn)
+    
+     }
+     board.push(row);
+   }
+   
+   
     return board
   }
 
